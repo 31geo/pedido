41ee14cdd7f3a10a52d7d567c723bbb4fbdba1b0
Geordy <tchinchayluisge@gmail.com>
2026-02-08 01:43:16 -0500

    fix test to not require database connection

--- src/test/java/com/tantalean/pedido/PedidoApplicationTests.java
+++ src/test/java/com/tantalean/pedido/PedidoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.tantalean.pedido;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PedidoApplicationTests {
 
 	@Test
